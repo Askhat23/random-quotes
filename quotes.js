edfabc2c30bab8608e158aad83d6f8f5b60d5ec5
Askhat Jussupov <acxatjussupov@gmail.com>
2025-11-10 16:55:13 +0500

    Splitting app into the JS modules

---
+++ quotes.js
@@ -0,0 +1,47 @@
+const quotes = [
+  {
+    quote: 'The best way to get started is to quit talking and begin doing',
+    author: 'Walt Disney',
+  },
+  {
+    quote: "Don't let yesterday take up too much of today",
+    author: 'Will Rogers',
+  },
+  {
+    quote: "It's not whether you get knocked down, it's whether you get up",
+    author: 'Vince Lombardi',
+  },
+  {
+    quote:
+      'If you are working on something exciting, it will keep you motivated',
+    author: 'Steve Jobs',
+  },
+  {
+    quote:
+      "The harder you work for something, the greater you'll feel when you achieve it",
+    author: 'Unknown author',
+  },
+  {
+    quote:
+      'Success is not final; failure is not fatal: It is the courage to continue that counts',
+    author: 'Winston Churchill',
+  },
+  {
+    quote: 'Your time is limited, so don’t waste it living someone else’s life',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'In the middle of every difficulty lies opportunity',
+    author: 'Albert Einstein',
+  },
+  {
+    quote: 'Believe you can and you’re halfway there',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    quote: 'If you want to achieve greatness, stop asking for permission',
+    author: 'Anonymous',
+  },
+];
+
+export default quotes;
