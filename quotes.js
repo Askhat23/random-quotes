c52ebe05a075f3902d44ff04bc403dd3d0369287
Askhat Jussupov <acxatjussupov@gmail.com>
2025-11-10 21:07:12 +0500

    Show favorite quote below current quote

--- quotes.js
+++ quotes.js
@@ -2,45 +2,55 @@ const quotes = [
   {
     quote: 'The best way to get started is to quit talking and begin doing',
     author: 'Walt Disney',
+    isFavorite: false,
   },
   {
     quote: "Don't let yesterday take up too much of today",
     author: 'Will Rogers',
+    isFavorite: false,
   },
   {
     quote: "It's not whether you get knocked down, it's whether you get up",
     author: 'Vince Lombardi',
+    isFavorite: false,
   },
   {
     quote:
       'If you are working on something exciting, it will keep you motivated',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote:
       "The harder you work for something, the greater you'll feel when you achieve it",
     author: 'Unknown author',
+    isFavorite: false,
   },
   {
     quote:
       'Success is not final; failure is not fatal: It is the courage to continue that counts',
     author: 'Winston Churchill',
+    isFavorite: false,
   },
   {
     quote: 'Your time is limited, so don’t waste it living someone else’s life',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote: 'In the middle of every difficulty lies opportunity',
     author: 'Albert Einstein',
+    isFavorite: false,
   },
   {
     quote: 'Believe you can and you’re halfway there',
     author: 'Theodore Roosevelt',
+    isFavorite: false,
   },
   {
     quote: 'If you want to achieve greatness, stop asking for permission',
     author: 'Anonymous',
+    isFavorite: false,
   },
 ];
 
