793a4fec171f31e913938ae556ecc3061d064ecd
Askhat Jussupov <acxatjussupov@gmail.com>
2025-11-10 15:40:55 +0500

    Selecting a random quote when clicking in the button

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const quotes = [
   "Don't let yesterday take up too much of today. – Will Rogers",
   "It's not whether you get knocked down, it's whether you get up. – Vince Lombardi",
   'If you are working on something exciting, it will keep you motivated. – Steve Jobs',
-  "The harder you work for something, the greater you'll feel when you achieve it.",
+  "The harder you work for something, the greater you'll feel when you achieve it",
 ];
 
 const quoteElement = document.getElementById('quote');
