06af3ee3f2e8c72e246c6dc9b63e6ff8e903a0a6
Askhat Jussupov <acxatjussupov@gmail.com>
2025-11-13 18:57:16 +0500

    Refactor quote module by moving quotes and currentQuote to index module

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import { handleQuote } from './src/handlers/quote.js';
+import quotes from './src/data/quotes.js';
+
+let currentQuote = null;
+
+function setCurrentQuote(quote) {
+  currentQuote = quote;
+}
 
 const button = document.getElementById('new-quote');
-button.addEventListener('click', handleQuote);
+button.addEventListener('click', () => handleQuote(quotes, setCurrentQuote));
+
+export { currentQuote };
