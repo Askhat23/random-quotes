39f618ebf868ba3eef59a421464772e42f5f69b6
Askhat Jussupov <acxatjussupov@gmail.com>
2025-11-13 17:19:55 +0500

    Refactoring - create favorites and quote modules

---
+++ src/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    text: 'The best way to get started is to quit talking and begin doing',
+    author: 'Walt Disney',
+    isFavorite: false,
+  },
+  {
+    text: "Don't let yesterday take up too much of today",
+    author: 'Will Rogers',
+    isFavorite: false,
+  },
+  {
+    text: "It's not whether you get knocked down, it's whether you get up",
+    author: 'Vince Lombardi',
+    isFavorite: false,
+  },
+  {
+    text: 'If you are working on something exciting, it will keep you motivated',
+    author: 'Steve Jobs',
+    isFavorite: false,
+  },
+  {
+    text: "The harder you work for something, the greater you'll feel when you achieve it",
+    author: 'Unknown author',
+    isFavorite: false,
+  },
+  {
+    text: 'Success is not final; failure is not fatal: It is the courage to continue that counts',
+    author: 'Winston Churchill',
+    isFavorite: false,
+  },
+  {
+    text: 'Your time is limited, so don’t waste it living someone else’s life',
+    author: 'Steve Jobs',
+    isFavorite: false,
+  },
+  {
+    text: 'In the middle of every difficulty lies opportunity',
+    author: 'Albert Einstein',
+    isFavorite: false,
+  },
+  {
+    text: 'Believe you can and you’re halfway there',
+    author: 'Theodore Roosevelt',
+    isFavorite: false,
+  },
+  {
+    text: 'If you want to achieve greatness, stop asking for permission',
+    author: 'Anonymous',
+    isFavorite: false,
+  },
+];
+
+export default quotes;
