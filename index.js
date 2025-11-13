2636d77c4405719181ead73e1c9e1d8c70364a4e
Askhat Jussupov <acxatjussupov@gmail.com>
2025-11-13 19:53:06 +0500

    Finish refactoring of all modules by moving app logic to the index module

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { handleQuote } from './src/handlers/quote.js';
 import quotes from './src/data/quotes.js';
+import { toggleFavorite } from './src/handlers/favorites.js';
+import { hideFavoriteBtn } from './src/handlers/favorites.js';
 
 let currentQuote = null;
 
@@ -7,7 +9,14 @@ function setCurrentQuote(quote) {
   currentQuote = quote;
 }
 
+const favoritesContainer = document.getElementById('favorites-container');
+const favoriteBtn = document.getElementById('favorite-btn');
+hideFavoriteBtn(favoriteBtn);
+favoriteBtn.addEventListener('click', () =>
+  toggleFavorite(currentQuote, favoriteBtn, favoritesContainer)
+);
+
 const button = document.getElementById('new-quote');
 button.addEventListener('click', () => handleQuote(quotes, setCurrentQuote));
 
-export { currentQuote };
+export { favoriteBtn };
